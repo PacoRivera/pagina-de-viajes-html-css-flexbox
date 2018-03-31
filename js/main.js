5805cbbe22a9c0a0a688a57fcff20a2082e75ddb
PacoRivera <franciscotcrivera@gmail.com>
2018-03-30 23:41:44 -0600

    iniciado proyecto

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+function load(seleccionElemento, element)
+{
+    var url = seleccionElemento.getAttribute('data-content-url');
+    req = new XMLHttpRequest();
+    req.open("GET", url, false);
+    req.send(null);
+
+    element.innerHTML = req.responseText; 
+    $("li").removeClass("active");
+    seleccionElemento.classList.add('active');
+}
+
