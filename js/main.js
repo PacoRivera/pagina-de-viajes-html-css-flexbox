7d6714f2fbcca7a9b6164e9aedf454f0afeb8e8d
PacoRivera <franciscotcrivera@gmail.com>
2018-03-31 14:37:42 -0600

    Agregado header

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 
 
-function load(seleccionElemento, element)
-{
-    var url = seleccionElemento.getAttribute('data-content-url');
-    req = new XMLHttpRequest();
-    req.open("GET", url, false);
-    req.send(null);
+// function load(seleccionElemento, element)
+// {
+//     var url = seleccionElemento.getAttribute('data-content-url');
+//     req = new XMLHttpRequest();
+//     req.open("GET", url, false);
+//     req.send(null);
 
-    element.innerHTML = req.responseText; 
-    $("li").removeClass("active");
-    seleccionElemento.classList.add('active');
-}
+//     element.innerHTML = req.responseText; 
+//     $("li").removeClass("active");
+//     seleccionElemento.classList.add('active');
+// }
 
